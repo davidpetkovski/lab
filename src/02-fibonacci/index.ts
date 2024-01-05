a0cda8dcf70ef3b40957015aea898f8937ffdf9f
davidpetkovski <petkovski.a22007@next.edu.mk>
2024-01-05 17:47:58 +0100

    second exercise done

--- src/02-fibonacci/index.ts
+++ src/02-fibonacci/index.ts
@@ -1,1 +1,21 @@
-export function fibonacci() {}
+export function fibonacci(term: number): Array<number> {
+  const arr: Array<number> = []
+
+  if (term <= 0) {
+    return arr
+  }
+
+  if (term > 0) {
+    arr.push(0)
+  }
+
+  if (term > 1) {
+    arr.push(1)
+  }
+
+  for (let i = 2; i < term; i++) {
+    arr.push(arr[i - 1] + arr[i - 2])
+  }
+
+  return arr
+}
