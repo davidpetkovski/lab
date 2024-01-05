42447ac407cd74ca7f738dc1756a123b4459d14f
davidpetkovski <petkovski.a22007@next.edu.mk>
2024-01-05 18:14:47 +0100

    fourth exercise done

--- src/04-pascals-triangle/index.ts
+++ src/04-pascals-triangle/index.ts
@@ -1,1 +1,17 @@
-export function getPascalsTriangleRow(rowIndex) {}
+export function getPascalsTriangleRow(rowIndex: number): Array<number> {
+  const arr: number[] = []
+
+  for (let i = 0; i <= rowIndex; i++) {
+    const newRow: number[] = []
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        newRow.push(1)
+      } else {
+        newRow.push(arr[j - 1] + arr[j])
+      }
+    }
+    arr.splice(0, arr.length, ...newRow)
+  }
+
+  return arr
+}
