dedeed7682d24266efe80a064f95dc48c4cd03bf
davidpetkovski <petkovski.a22007@next.edu.mk>
2024-01-05 18:08:36 +0100

    third exercise done

--- src/03-email-extractor/index.ts
+++ src/03-email-extractor/index.ts
@@ -1,1 +1,19 @@
-export function extractEmails(users) {}
+interface User {
+  id: number
+  name: string
+  email: string
+  age: number
+}
+
+export function extractEmails(users: Array<User>): Array<string> {
+  const arr: Array<string> = []
+  if (users.length < 1) {
+    return arr
+  }
+  users.forEach((user) => {
+    if (user && !arr.includes(user.email)) {
+      arr.push(user.email)
+    }
+  })
+  return arr
+}
