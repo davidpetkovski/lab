bcb01faa0ba13384d75688129c2a407e376fc2bb
davidpetkovski <petkovski.a22007@next.edu.mk>
2024-01-05 17:30:45 +0100

    first exercise done

--- src/01-sum-of-multiples/index.ts
+++ src/01-sum-of-multiples/index.ts
@@ -1,1 +1,10 @@
-export function sumOfMultiples(multiple, max): {}
+export function sumOfMultiples(multiple: number, max: number): number {
+  let i: number = 1
+  let sum: number = 0
+  while (i * multiple <= max) {
+    sum += i * multiple
+    i++
+  }
+
+  return sum
+}
